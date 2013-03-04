040bbc9d15ab99113aaa1480a9f77903c3d13a98
rewtnull <marcus.hoffren@gmail.com>
2013-03-04 19:44:30 +0100

    Version 0.7

--- bdl.sh
+++ bdl.sh
@@ -8,14 +8,8 @@
 # There is NO WARRANTY, to the extent permitted by law.
 #
 
-scrname="Bash Desktop Launcher"
-scrver="0.6"
-scrauth="Marcus Hoffren"
-authnick="dMG/Up Rough"
-scrcontact="marcus.hoffren@gmail.com"
-
 usage() {
-    echo "Usage: ${0} [--help|-h] [--version|-v] [--sessions|-s] [arg]"
+    echo "Usage: ${0##*/} [--help|-h] [--version|-v] [--sessions|-s] [arg]"
     echo ""
     echo "-h, --help        Display this help."
     echo "-v, --version     Display version and exit."
@@ -26,6 +20,11 @@ usage() {
 }
 
 version() {
+    local scrname="Bash Desktop Launcher"
+    local scrver="0.7"
+    local scrauth="Marcus Hoffren"
+    local authnick="dMG/Up Rough"
+    local scrcontact="marcus.hoffren@gmail.com"
     echo "${scrname} v${scrver}"
     echo "Copyright (C) 2013 ${scrauth} <${authnick}>."
     echo "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
@@ -36,53 +35,44 @@ version() {
     echo ""
 }
 
+error() {
+    { echo -e "${@}" 1>&2; usage; exit 1; }
+}
+
 args() {
     case ${1} in
 	--version|-v)
-		version # fc
+		version
 		exit 0;;
 	--help|-h)
-		usage # fc
+		usage
 		exit 0;;
 	--sessions|-s)
 		echo "Installed window managers:"
-		echo "$(xsessions "${sessions[@]}" "Default")" # fc
+		echo "$(xsessions "${sessions[@]}" "Default")"
 		exit 0;;
 	*)
-		usage # fc
-		exit 1;;
+		error;;
     esac
 }
-
 menu() {
     dialog --hfile "bdl-help.txt" --stdout --no-cancel --no-ok --no-tags --backtitle "(F1) ${scrname} v${scrver}" \
-	--menu "Window Managers:" 30 40 20 $(xsessions "$@") # ec, fc
+	--menu "Window Managers:" 30 40 20 $(xsessions "$@")
 }
 
 # Echo numbered list of arguments
 xsessions() {
     local count="1"
     for arg in ${@}; do
-	echo "${count} $(collapse "${arg}")" # fc
+	echo "${count} ${arg##*/}"
 	let count++
     done
-}
-
-# Collapse argument. Same as basename *stub
-collapse() {
-    local name="${1##*/}"
-    echo "${name%${2}}"
-}
-
-# Expand argument. Reversed basename *stub
-expand() {
-    type -p "${1}"
 }
 
 # Expands first of found arguments. Accept any, return first
 expand_either() {
     for arg in ${@}; do
-	local result=$(expand "${arg}") # fc
+	local result=$(type -p "${arg}")
 	[[ "${result}" != "" ]] && { echo "${result}"; return 0; }
     done
     return 1
@@ -94,45 +84,34 @@ gtoe() {
 
 # Void
 sanity_check() {
-    # Do we run this on Bash v4.1+?
-    [[ "${BASH_VERSION}" < 4.1 ]] && { echo -e "${scrname} requires \033[1mbash v4.1 or newer\033[m."; exit 1; }
-    # Is dialog installed?
-    local dialog_path=$(expand dialog) # fc
-    [[ ${dialog_path} == "" ]] && { echo -e "dialog v1.2 or newer \033[1mrequired.\033[m"; exit 1; }
-    # Dialog version check
-    gtoe "$(dialog --version | head -c12)" "Version: 1.2" || { # fc, ec
-    echo -e "\033[1mdialog\033[m version requirement \033[1m>= 1.2\033[m not met. Aborting."; exit 1;
-    }
-    # Does startx or xinit exist?
-    start=$(expand_either startx xinit) # fc, gvd
-    [[ "${start}" == "" ]] && { echo -e "startx or xinit \033[1mnot found.\033[m Is Xorg installed?"; exit 1; }
-    # Do we have any sessions?
-    sessions=(/etc/X11/Sessions/*) # Expand path into array, gvd
-    (( "${#sessions[@]}" == "0" )) && { echo -e "No sessions found. Is /etc/X11/Sessions empty?"; exit 1; }
+    [[ "${BASH_VERSION}" < 4.1 ]] && error "${0##*/} requires \033[1mbash v4.1 or newer\033[m."
+    [[ $(type -p dialog) == "" ]] && error "dialog v1.2 or newer \033[1mrequired.\033[m"
+    gtoe "$(dialog --version | head -c12)" "Version: 1.2" || error "\033[1mdialog\033[m version requirement \033[1m>= 1.2\033[m not met. Aborting."
+    start=$(expand_either startx xinit)
+    [[ "${start}" == "" ]] && error "startx or xinit \033[1mnot found.\033[m Is Xorg installed?"
+    sessions=(/etc/X11/Sessions/*) # Expand path into array
+    (( "${#sessions[@]}" == "0" )) && error "No sessions found. Is /etc/X11/Sessions empty?"
 }
 
-sanity_check # fc
+sanity_check
 case ${#} in # Number of arguments
     0)
-	index=$(menu "${sessions[@]}" "Default" "Quit") # gvd, fc
+	index=$(menu "${sessions[@]}" "Default" "Quit")
 	(( "${index}" == "${#sessions[@]}" + 2 )) && exit 0;; # Number of array elements + 2 = Quit
     1)
 	if [[ "${1}" =~ ^- ]]; then # Does the first argument start with a - ?
-	    args "${1}" # fc
+	    args "${1}"
 	elif [[ "${1}" =~ ^[[:digit:]]+$ ]]; then # Is the first argument a number?
-	    index="${1}" # gvd
+	    index="${1}"
 	    if (( "${index}" >= "${#sessions[@]}" + 2 )); then # Limit arg
-		echo -e "\033[1m${1}\033[m is not a valid argument. \033[1m${0} --sessions\033[m to list sessions."
-		exit 1
+		error "\033[1m${1}\033[m is not a valid argument. \033[1m${0} --sessions\033[m to list sessions."
 	    fi
 	else
-	    usage # fc
-	    exit 1
+	    usage; exit 1
         fi;;
     *)
-	usage # fc
-	exit 1;;
+	error;;
 esac
 
 let index-- # Make index zero based
-echo "${start}" "${sessions[${index}]}" # Launch Xorg
+exec "${start}" "${sessions[${index}]}" # Launch Xorg
